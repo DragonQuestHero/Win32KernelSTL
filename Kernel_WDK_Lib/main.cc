dc35c66dad1999f83c55bf036b6d7d92c8c1229b
新手村的满级史莱姆 <DragonQuestMaou@gmail.com>
2018-10-26 21:12:54 +0800

    #Comment

--- Kernel_WDK_Lib/main.cc
+++ Kernel_WDK_Lib/main.cc
@@ -2,6 +2,9 @@
 #undef _HAS_EXCEPTIONS
 #define _HAS_EXCEPTIONS 0
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
 
 
 void DriverUnload(PDRIVER_OBJECT drive_object)
@@ -13,9 +16,18 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT drive_object, PUNICODE_STRING pat
 {
 	drive_object->DriverUnload = DriverUnload;
 
-	std::string a = "1";
+	/*std::string a = "1";
 	int b = std::stoi(a);
-	DbgPrint("%s", a.data());
+	DbgPrint("%s", a.data());*/
+
+	std::vector<int> _vector;
+	_vector.push_back(1);
+	_vector.push_back(3);
+	std::sort(_vector.begin(),_vector.end());
+	for (auto i:_vector)
+	{
+		
+	}
 
 
 	return STATUS_SUCCESS;
